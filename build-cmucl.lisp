a5dd4dd13331ea69290b1a3b7400e8e26718da3e
Elias Pipping <elias.pipping@fu-berlin.de>
2016-09-15 16:47:18 +0000

    CMU CL: Do not print herald

--- build-cmucl.lisp
+++ build-cmucl.lisp
@@ -4,4 +4,6 @@
 
 (setf uiop:*image-dumped-p* :executable)
 (setf uiop:*image-entry-point* #'echo:main)
-(save-lisp "echo" :executable t :init-function #'uiop:restore-image)
+(save-lisp "echo" :executable t
+           :init-function #'uiop:restore-image
+           :print-herald nil)
